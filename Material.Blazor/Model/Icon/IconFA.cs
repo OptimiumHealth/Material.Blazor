62352194ebf11c6d2aaf86499e2f0226dd280081
Simon Ziegler <simon@dioptra.tech>
2021-02-08 12:39:21 +0000

    Review Updates
    SZ and MS to walk through review, in particular for docs. Once review is complete SL to upgrade Snackbar configuration and settings.

--- Material.Blazor/Model/Icon/IconFA.cs
+++ Material.Blazor/Model/Icon/IconFA.cs
@@ -15,7 +15,7 @@ namespace Material.Blazor.Internal
         public string Text => "";
 
         
-        private readonly Dictionary<string, object> _attributes = new Dictionary<string, object>();
+        private readonly Dictionary<string, object> _attributes = new();
         /// <inheritdoc />
         public IDictionary<string, object> Attributes => _attributes;
 
